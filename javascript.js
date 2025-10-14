77f8842c328ed4dced0b9c701bfc943d9ee6df4b
Yasini21 <yasinivijayakumar021@gmail.com>
2025-10-14 16:13:04 +0530

    Updated index

---
+++ javascript.js
@@ -0,0 +1,57 @@
+// console.log("testing")
+// console.lob(a);
+// var a=6;
+// console.lob(a);
+// let a=6;
+
+
+// Non-primitive
+// let object1={age:33};
+// let object2=object1;
+// object1.age=22;
+// console.log(object2.age);
+
+//if-else
+    // let a=true;
+    // let b=false;
+    // if(a||b){
+    //     console.log("Hello");
+    // }
+
+
+//function
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum(10,12));
+
+// function greet(){
+//     return "Hello Js";
+// }
+// console.log(greet());
+
+// const sum1 =(a,b,c)=>{
+//   console.log(a+b+c);
+// }
+// sum1(11,12,13);
+
+// const sum2=(a,b,c)=>{
+//     return a+b+c;
+// }
+// console.log(sum2(10,25,40));
+
+// const add=(a,b)=>a+b
+// console.log(add(2,4));
+
+// function Person() {
+//   this.age = 0;
+
+//   setInterval(() => {
+//     this.age++; // 'this' refers to Person object
+//     console.log(this.age);
+//   }, 1000);
+// }
+
+// const p = new Person();
+
+
